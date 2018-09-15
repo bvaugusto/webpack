76b365975e5c779a6ff93d09f7b6b7b8e59d21be
Bruno Augusto <bvaugusto@gmail.com>
2018-09-15 18:38:33 -0300

    Add bootstrap jquery

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+
+import  'jquery/dist/jquery.js';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
 import './assets'
 
 const produto = {
